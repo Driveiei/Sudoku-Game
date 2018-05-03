f6d6d9afe9869fca6ce0825fb320302b01706a65
Kornphon Noiprasert <konphon22@hotmail.com>
2018-05-03 16:00:25 +0700

    Complete Random Logic

--- src/logic/RandomNumber.java
+++ src/logic/RandomNumber.java
@@ -17,15 +17,15 @@ public class RandomNumber implements Runnable {
 
 	@Override
 	public void run() {
-		int size = (table.getSize() * table.getSize()) ; //9
+		int size = (table.getSize() * table.getSize()); // 9
 		for (int grid = 0; grid < size; grid++) {
 			table.getList().add(new GridManager(grid, true));// create 9 grids.
-			createRandomSet(createNumberSet(size), grid,size); // create grid 0 1 2 3 4 5 6 7 8
+			createRandomSet(createNumberSet(size), grid, size); // create grid 0 1 2 3 4 5 6 7 8
 		}
-		
-		for(int i = 0; i< size; i++) {
-			System.out.println("Block "+i);
-			for(int y = 0; y<size;y++) {
+
+		for (int i = 0; i < size; i++) {
+			System.out.println("Block " + i);
+			for (int y = 0; y < size; y++) {
 				System.out.println(table.getList().get(i).getList().get(y).getNumber());
 			}
 		}
@@ -41,57 +41,59 @@ public class RandomNumber implements Runnable {
 		return merge;
 	}
 
-	public void createRandomSet(List<Integer> setOfNumber, int numberGrid,int size) {
-		int realSize = table.getSize();//3
+	public void createRandomSet(List<Integer> setOfNumber, int numberGrid, int size) {
+		int realSize = table.getSize();// 3
 		int column;
 		int row;
 		List<Integer> duplicateSet = new ArrayList<Integer>();
 		List<Integer> collecting = new ArrayList<Integer>();
-		for(int box = 0; box < size; box++) {
-			
-			column = realSize*(numberGrid%realSize)+box%realSize;
-			row = realSize*(numberGrid/realSize)+box/realSize;
-			
-			duplicateSet.addAll(mergeDuplicateList(table.duplicateColumn(numberGrid, column),table.duplicateRow(numberGrid, row)));
-			
-			for(Integer out : duplicateSet) {
-				if(setOfNumber.contains(out)) {
+		for (int box = 0; box < size; box++) {
+
+			column = realSize * (numberGrid % realSize) + box % realSize;
+			row = realSize * (numberGrid / realSize) + box / realSize;
+
+			duplicateSet.addAll(
+					mergeDuplicateList(table.duplicateColumn(numberGrid, column), table.duplicateRow(numberGrid, row)));
+
+			for (Integer out : duplicateSet) {
+				if (setOfNumber.contains(out)) {
 					setOfNumber.remove(setOfNumber.indexOf(out));
 					collecting.add(out);
 				}
 			}
-			
+
 			try {
 				int cursor = rand.nextInt(setOfNumber.size()); // random 0-8 position
 				int target = setOfNumber.get(cursor);
-				table.getList().get(numberGrid).getList().add(new BoxManager(target,true,true));
+				table.getList().get(numberGrid).getList().add(new BoxManager(target, true, true));
 				setOfNumber.remove(setOfNumber.indexOf(target));
 				setOfNumber.addAll(collecting);
 			} catch (IllegalArgumentException ex) {
-				box = undoCreateGrid(numberGrid,size);				
+				box = undoCreateGrid(numberGrid, size, realSize);
+				table.getList().get(numberGrid).getList().clear();
 				setOfNumber.clear();
 				setOfNumber.addAll(createNumberSet(size));
-				table.getList().get(numberGrid).getList().clear();
 			}
 			collecting.clear();
 			duplicateSet.clear();
 		}
 	}
 
-	public int undoCreateGrid(int numberGrid,int size) {
-		if(numberGrid%3 ==1) {
-			table.getList().get(numberGrid-1).getList().clear();
-			createRandomSet(createNumberSet(size), numberGrid-1,size);
+	public int undoCreateGrid(int numberGrid, int size, int realSize) {
+		int remainder = numberGrid % realSize;
+		if (remainder == 1) {
+			table.getList().get(numberGrid - remainder).getList().clear();
+			createRandomSet(createNumberSet(size), numberGrid - remainder, size);
 		}
-		if(numberGrid %3 == 2) {
-			table.getList().get(numberGrid-2).getList().clear();
-			table.getList().get(numberGrid-1).getList().clear();
-			createRandomSet(createNumberSet(size), numberGrid-2,size); 
-			createRandomSet(createNumberSet(size), numberGrid-1,size); 
+		if (numberGrid % realSize == 2) {
+			table.getList().get(numberGrid - remainder).getList().clear();
+			table.getList().get(numberGrid - (remainder - 1)).getList().clear();
+			createRandomSet(createNumberSet(size), numberGrid - remainder, size);
+			createRandomSet(createNumberSet(size), numberGrid - (remainder - 1), size);
 		}
 		return -1;
 	}
-	
+
 	public List<Integer> createNumberSet(int size) {
 		List<Integer> number = new ArrayList<Integer>();
 		for (int i = 1; i <= size; i++) {
