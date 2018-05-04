614672f0965c1e70c7317ba509cd2aa24a8f02e7
Kornphon Noiprasert <konphon22@hotmail.com>
2018-05-04 12:34:02 +0700

    Finish logic random part

--- src/logic/LogicMain.java
+++ src/logic/LogicMain.java
@@ -3,7 +3,7 @@ package logic;
 public class LogicMain {
 
 	public static void main(String[] args) {
-		Table table = new Table(3); // 3 or 4
+		Table table = new Table(4); // 3 or 4
 		RandomNumber random = new RandomNumber(table);
 		random.run();
 	}
