49d6d396bdd1b9bc7b502c94994b07f85024e0ae
Kornphon Noiprasert <konphon22@hotmail.com>
2018-05-04 12:51:20 +0700

    Starter for enum and tableUI<Put number to the table>

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,35 @@
 package application;
 
-public class Main {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import javafx.scene.layout.BorderPane;
+
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	
+	public static void main(String[] arg) {
+		launch(arg);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		try {
+		BorderPane pane = (BorderPane)FXMLLoader.load(getClass().getResource("GridUI.fxml"));	
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+		stage.setTitle("GridShow");
+		stage.show();
+		}
+		catch(IOException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 
 }
